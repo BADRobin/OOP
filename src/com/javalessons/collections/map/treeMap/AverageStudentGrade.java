578bd509ab515e2bee93f01ded631b17043edfe8
BADrobin <olegbryl@gmail.com>
2022-10-22 20:25:57 +0200

    new11

---
+++ src/com/javalessons/collections/map/treeMap/AverageStudentGrade.java
@@ -0,0 +1,35 @@
+package com.javalessons.collections.map.treeMap;
+
+public class AverageStudentGrade implements Comparable<AverageStudentGrade>{
+    private final String name;
+    private final float averageGrade;
+
+    public AverageStudentGrade(String name, float averageGrade) {
+        this.name = name;
+        this.averageGrade = averageGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getAverageGrade() {
+        return averageGrade;
+    }
+
+    @Override
+    public int compareTo(AverageStudentGrade that) {
+        if (this.averageGrade < that.getAverageGrade()){
+            return 1;
+        }
+        if (this.averageGrade > that.getAverageGrade()){
+            return -1;
+        }
+        return name.compareTo(that.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Student name: " + name + "  " + "Grade: " + averageGrade;
+    }
+}
--- src/com/javalessons/collections/map/treeMap/TreeMapRunner.java
+++ src/com/javalessons/collections/map/treeMap/TreeMapRunner.java
@@ -1,10 +1,70 @@
 package com.javalessons.collections.map.treeMap;
 
+import java.util.*;
+
 public class TreeMapRunner {
     public static void main(String[] args) {
-
+        NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = createGrades();
+        printGrades(grades, true);
     }
-    private static void createGrades(){
+    private static void printGrades(Map<AverageStudentGrade, Set<SubjectGrade>> grades, boolean printValue){
+        Set<AverageStudentGrade> averageGrades = grades.keySet();
+        for (AverageStudentGrade gr : averageGrades){
+            System.out.println(gr);
+            if (printValue){
+                System.out.println(grades.get(gr));
+            }
+        }
+    }
+    private static NavigableMap<AverageStudentGrade, Set<SubjectGrade>> createGrades(){
+        Set<SubjectGrade> olehGrade = new HashSet<>();
+        olehGrade.add(new SubjectGrade("Math",  89));
+        olehGrade.add(new SubjectGrade("Physic",  79));
+        olehGrade.add(new SubjectGrade("History",  95));
+        olehGrade.add(new SubjectGrade("Literature",  69));
+        olehGrade.add(new SubjectGrade("Chemistry",  80));
 
+        Set<SubjectGrade> jamesGrade = new HashSet<>();
+        jamesGrade.add(new SubjectGrade("Math",  75));
+        jamesGrade.add(new SubjectGrade("Physic",  80));
+        jamesGrade.add(new SubjectGrade("History",  55));
+        jamesGrade.add(new SubjectGrade("Literature",  70));
+        jamesGrade.add(new SubjectGrade("Chemistry",  80));
+
+        Set<SubjectGrade> antonyGrade = new HashSet<>();
+        antonyGrade.add(new SubjectGrade("Math",  93));
+        antonyGrade.add(new SubjectGrade("Physic",  91));
+        antonyGrade.add(new SubjectGrade("History",  92));
+        antonyGrade.add(new SubjectGrade("Literature",  78));
+        antonyGrade.add(new SubjectGrade("Chemistry",  88));
+
+        Set<SubjectGrade> viktoriaGrade = new HashSet<>();
+        viktoriaGrade.add(new SubjectGrade("Math",  73));
+        viktoriaGrade.add(new SubjectGrade("Physic",  65));
+        viktoriaGrade.add(new SubjectGrade("History",  75));
+        viktoriaGrade.add(new SubjectGrade("Literature",  66));
+        viktoriaGrade.add(new SubjectGrade("Chemistry",  50));
+
+        Set<SubjectGrade> alinaGrade = new HashSet<>();
+        alinaGrade.add(new SubjectGrade("Math",  90));
+        alinaGrade.add(new SubjectGrade("Physic",  70));
+        alinaGrade.add(new SubjectGrade("History",  78));
+        alinaGrade.add(new SubjectGrade("Literature",  88));
+        alinaGrade.add(new SubjectGrade("Chemistry",  89));
+
+        NavigableMap<AverageStudentGrade, Set<SubjectGrade >> map = new TreeMap<>();
+        map.put(new AverageStudentGrade("Oleh", calcAverage(olehGrade) ), olehGrade);
+        map.put(new AverageStudentGrade("James", calcAverage(jamesGrade) ), jamesGrade);
+        map.put(new AverageStudentGrade("Antony", calcAverage(antonyGrade) ), antonyGrade);
+        map.put(new AverageStudentGrade("Viktoria", calcAverage(viktoriaGrade) ), viktoriaGrade);
+        map.put(new AverageStudentGrade("Alina", calcAverage(alinaGrade) ), alinaGrade);
+        return map;
+    }
+    private static float calcAverage(Set<SubjectGrade> grades){
+        float sum = 0f;
+        for (SubjectGrade sg: grades){
+            sum += sg.getGrade();
+        }
+        return sum/grades.size();
     }
 }
